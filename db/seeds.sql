10ae643e28f3c21a68512651b0039ec48b430717
Ben Sojka <bensojka@gmail.com>
2017-10-05 11:08:43 -0500

    not done

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name) VALUES ('Firey Hawaiian Burger');
+INSERT INTO burgers (burger_name) VALUES ('Blackened Bleu Cheese & Bacon Burger');
+INSERT INTO burgers (burger_name) VALUES ('Gourmet Steakhouse Burger');
