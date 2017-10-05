00b39c8ac3f56f838a29d3d13b7233f0274c48a1
Ben Sojka <bensojka@gmail.com>
2017-10-05 16:04:03 -0500

    fixed connection

--- server.js
+++ server.js
@@ -3,24 +3,24 @@ var methodOverride = require("method-override");
 var bodyParser = require("body-parser");
 var exphbs = require('express-handlebars');
 
+var port = process.env.PORT || 8080;
+
 var app = express();
 
-//Serve static content for the app from the "public" directory in the application directory.
-app.use(express.static(process.cwd() + '/public'));
+// Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static("public"));
 
-app.use(bodyParser.urlencoded({
-	extended: false
-}))
+app.use(bodyParser.urlencoded({ extended: false }));
+
 // override with POST having ?_method=DELETE
 app.use(methodOverride('_method'));
 
-app.engine('handlebars', exphbs({
-    defaultLayout: 'main',
-}));
-app.set('view engine', 'handlebars');
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
 
-var routes = require('./controllers/burgers_controller.js');
-app.use('/', routes);
+// Import routes and give the server access to them.
+var routes = require("./controllers/burgers_controller.js");
 
-var port = process.env.PORT || 3000;
+app.use("/", routes);
+
 app.listen(port);
